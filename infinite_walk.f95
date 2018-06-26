cdefae17881b0ad5c2b3c83741521f1724c9847d
JackTapay <36042773+JackTapay@users.noreply.github.com>
2018-06-26 15:26:11 -0400

    Update infinite_walk.f95

--- infinite_walk.f95
+++ infinite_walk.f95
@@ -1,181 +1,156 @@
-program test
-implicit none 
-integer, dimension(1000)::tau
-integer, dimension(1000)::position,position1,position2,position3,position4,position5,position6
-real,dimension(1000)::x,c,d,f,g,h,j
-real::y,a,b
-integer::n,e,q,w,r,t,u,i,m
+SUBROUTINE RandomNumber(a,n,x)
+	implicit none
+    INTEGER,INTENT(INOUT)::n
+    REAL,INTENT(INOUT)::x
+    REAL,INTENT(INOUT),dimension(1000):: a
+    do n=1,1000
+      call random_number(x)
+      a(n)=x
+    end do
+    RETURN
+END SUBROUTINE
     
-a=1.0/3.0
-b=2.0/3.0
-e=0
-
-do n=1,1000
-    tau(n)=n
-    call random_number(y)
-    x(n)=y
-end do
-
-do n=1,1000
-    tau(n)=n
-    call random_number(y)
-    c(n)=y
-end do
-
-do n=1,1000
-    tau(n)=n
-    call random_number(y)
-    d(n)=y
-end do
-
-do n=1,1000
-    tau(n)=n
-    call random_number(y)
-    f(n)=y
-end do
-
-do n=1,1000
-    tau(n)=n
-    call random_number(y)
-    g(n)=y
-end do
-
-do n=1,1000
-    tau(n)=n
-    call random_number(y)
-    h(n)=y
-end do
-
-do n=1,1000
-    tau(n)=n
-    call random_number(y)
-    j(n)=y
-end do
-
-do n=1,1000
-    if (x(n) .le. a) then
-        position(n)=e+1
-        e=position(n)
-    else if ((x(n) .gt. a) .and. (x(n) .le. b)) then
-        position(n)=e-1
-        e=position(n)
-    else
-        position(n)=e+0
-        e=position(n)
-    end if
-end do
-
-do n=1,1000
-    if (c(n) .le. a) then
-        position1(n)=q+1
-        q=position1(n)
-    else if ((c(n) .gt. a) .and. (c(n) .le. b)) then
-        position1(n)=q-1
-        q=position1(n)
-    else
-        position1(n)=q+0
-        q=position1(n)
-    end if
-end do
-
-do n=1,1000
-    if (d(n) .le. a) then
-        position2(n)=w+1
-        w=position2(n)
-    else if ((d(n) .gt. a) .and. (d(n) .le. b)) then
-        position2(n)=w-1
-        w=position2(n)
-    else
-        position2(n)=w+0
-        w=position2(n)
-    end if
-end do
-
-do n=1,1000
-    if (f(n) .le. a) then
-        position3(n)=r+1
-        r=position3(n)
-    else if ((f(n) .gt. a) .and. (f(n) .le. b)) then
-        position3(n)=r-1
-        r=position3(n)
-    else
-        position3(n)=r+0
-        r=position3(n)
-    end if
-end do
-
-do n=1,1000
-    if (g(n) .le. a) then
-        position4(n)=t+1
-        t=position4(n)
-    else if ((g(n) .gt. a) .and. (g(n) .le. b)) then
-        position4(n)=t-1
-        t=position4(n)
-    else
-        position4(n)=t+0
-        t=position4(n)
-    end if
-end do
-
-do n=1,1000
-    if (h(n) .le. a) then
-        position5(n)=u+1
-        u=position5(n)
-    else if ((h(n) .gt. a) .and. (h(n) .le. b)) then
-        position5(n)=u-1
-        u=position5(n)
-    else
-        position5(n)=u+0
-        u=position5(n)
-    end if
-end do
-
-do n=1,1000
-    if (j(n) .le. a) then
-        position6(n)=i+1
-        i=position6(n)
-    else if ((j(n) .gt. a) .and. (j(n) .le. b)) then
-        position6(n)=i-1
-        i=position6(n)
-    else
-        position6(n)=i+0
-        i=position6(n)
-    end if
-end do
-
-print*, "how many particles"
-read*,m
-print*, "you chose ", m,"particles"
- 
-if (m==1) then
+SUBROUTINE PositionCalc(a,n,y,g)
+	INTEGER,INTENT(INOUT) :: n,y
+    INTEGER,INTENT(INOUT),DIMENSION(1000)::g
+    REAL:: third,twothird
+    REAL,INTENT(INOUT), dimension(1000):: a
+    third=1.00/3.00
+    twothird=2.00/3.00
+    y=0
     do n=1,1000
-        print*, tau(n),position(n)
-    end do
-else if (m==2) then
-    do n=1,1000
-        print*, tau(n),position(n),position1(n)
-    end do
-else if (m==3) then
-    do n=1,1000
-        print*, tau(n),position(n),position1(n),position2(n)
-    end do
-else if (m==4) then
-    do n=1,1000
-        print*, tau(n),position(n),position1(n),position2(n),position3(n)
-    end do
-else if (m==5) then
-    do n=1,1000
-        print*, tau(n),position(n),position1(n),position2(n),position3(n),position4(n)
-    end do
-else if (m==6) then
-    do n=1,1000
-        print*, tau(n),position(n),position1(n),position2(n),position3(n),position4(n),position5(n)
-    end do
-else if (m==7) then
-    do n=1,1000
-        print*, tau(n),position(n),position1(n),position2(n),position3(n),position4(n),position5(n),position6(n)
-    end do
+    	if (a(n) .le. third) then
+        	g(n)=y+1
+        	y=g(n)
+    	else if ((a(n) .gt. third) .and. (a(n) .le. twothird)) then
+        	g(n)=y-1
+        	y=g(n)
+    	else
+        	g(n)=y+0
+        	y=g(n)
+    	end if
+	end do
+    RETURN
+END SUBROUTINE
+
+program RandomWalk
+implicit none
+integer::unit,n,y
+real::x
+integer,dimension(1000)::position1,position2,position3,position4,position5,position6,position7,position8
+real,dimension(1000)::random1,random2,random3,random4,random5,random6,random7,random8
+print*, "How many units?"
+read(*,*) unit
+if ((unit>10) .or. (unit<0)) then
+  print*, "You chose ", unit," units. That number of units exceeds the capabilities of the program."
 else
-    print*, "ERROR: TOO MANY PARTICLES"
-end if 
-end program test
+  print*, "You chose ", unit," units."
+end if
+open (unit=10,file="RandomWalk.txt")
+select case (unit)
+  case(1)
+  	CALL RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    do n=1,1000
+      write(10,*) n,position1(n)
+    end do
+  case(2)
+    call RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    call RandomNumber(random2,n,x)
+    call PositionCalc(random2,n,y,position2)
+    do n=1,1000
+      write(10,*) n,position1(n),position2(n)
+    end do
+  case(3)
+    call RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    call RandomNumber(random2,n,x)
+    call PositionCalc(random2,n,y,position2)
+    call RandomNumber(random3,n,x)
+    call PositionCalc(random3,n,y,position3)
+    do n=1,1000
+      write(10,*) n,position1(n),position2(n),position3(n)
+    end do
+  case(4)
+    call RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    call RandomNumber(random2,n,x)
+    call PositionCalc(random2,n,y,position2)
+    call RandomNumber(random3,n,x)
+    call PositionCalc(random3,n,y,position3)
+    call RandomNumber(random4,n,x)
+    call PositionCalc(random4,n,y,position4)
+    do n=1,1000
+      write(10,*) n,position1(n),position2(n),position3(n),position4(n)
+    end do
+  case(5)
+    call RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    call RandomNumber(random2,n,x)
+    call PositionCalc(random2,n,y,position2)
+    call RandomNumber(random3,n,x)
+    call PositionCalc(random3,n,y,position3)
+    call RandomNumber(random4,n,x)
+    call PositionCalc(random4,n,y,position4)
+    call RandomNumber(random5,n,x)
+    call PositionCalc(random5,n,y,position5)
+    do n=1,1000
+      write(10,*) n,position1(n),position2(n),position3(n),position4(n),position5(n)
+    end do
+  case(6)
+    call RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    call RandomNumber(random2,n,x)
+    call PositionCalc(random2,n,y,position2)
+    call RandomNumber(random3,n,x)
+    call PositionCalc(random3,n,y,position3)
+    call RandomNumber(random4,n,x)
+    call PositionCalc(random4,n,y,position4)
+    call RandomNumber(random5,n,x)
+    call PositionCalc(random5,n,y,position5)
+    call RandomNumber(random6,n,x)
+    call PositionCalc(random6,n,y,position6)
+    do n=1,1000
+      write(10,*) n,position1(n),position2(n),position3(n),position4(n),position5(n),position6(n)
+    end do
+  case(7)
+    call RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    call RandomNumber(random2,n,x)
+    call PositionCalc(random2,n,y,position2)
+    call RandomNumber(random3,n,x)
+    call PositionCalc(random3,n,y,position3)
+    call RandomNumber(random4,n,x)
+    call PositionCalc(random4,n,y,position4)
+    call RandomNumber(random5,n,x)
+    call PositionCalc(random5,n,y,position5)
+    call RandomNumber(random6,n,x)
+    call PositionCalc(random6,n,y,position6)
+    call RandomNumber(random7,n,x)
+    call PositionCalc(random7,n,y,position7)
+    do n=1,1000
+      write(10,*) n,position1(n),position2(n),position3(n),position4(n),position5(n),position6(n),position7(n)
+    end do
+  case(8)
+    call RandomNumber(random1,n,x)
+    call PositionCalc(random1,n,y,position1)
+    call RandomNumber(random2,n,x)
+    call PositionCalc(random2,n,y,position2)
+    call RandomNumber(random3,n,x)
+    call PositionCalc(random3,n,y,position3)
+    call RandomNumber(random4,n,x)
+    call PositionCalc(random4,n,y,position4)
+    call RandomNumber(random5,n,x)
+    call PositionCalc(random5,n,y,position5)
+    call RandomNumber(random6,n,x)
+    call PositionCalc(random6,n,y,position6)
+    call RandomNumber(random7,n,x)
+    call PositionCalc(random7,n,y,position7)
+    call RandomNumber(random8,n,x)
+    call PositionCalc(random8,n,y,position8)
+    do n=1,1000
+      write(10,*) n,position1(n),position2(n),position3(n),position4(n),position5(n),position6(n),position7(n),position8(n)
+    end do
+end select 
+end program RandomWalk
