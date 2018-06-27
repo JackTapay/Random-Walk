50014db8b41cad0847137eb81b3a01856dfa5cfe
JackTapay <36042773+JackTapay@users.noreply.github.com>
2018-06-27 11:51:28 -0400

    Update infinite_walk.f95

--- infinite_walk.f95
+++ infinite_walk.f95
@@ -4,11 +4,11 @@ integer :: position(2,1000),n,y,g,z ! The 2 used in the declaration can be chang
 real::x,onethird,twothird
 onethird=1.00/3.00
 twothird=2.00/3.00
-y=1
 do n=1,1000
 	position(1,n)=n
 end do
 do z=2,2 !Despite the fact that this do loop is redundant with just 1 "position column", the ending value ought to be modified to the same value as the one made on line 3
+	y=1
 	do n=1,1000
         	call random_number(x)
          	if (x .le. onethird) then
