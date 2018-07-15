243002c23e959552dab3a485eb6b34459d8bd384
JackTapay <36042773+JackTapay@users.noreply.github.com>
2018-07-15 10:20:42 -0400

    Update RandomWalk.f95

--- RandomWalk.f95
+++ RandomWalk.f95
@@ -1,14 +1,14 @@
 program RandomWalk
 implicit none
-integer :: position(2,1000),n,y,g,z ! The 2 used in the declaration can be changed in order to create n-number of walkers
+integer :: position(2,1000),n,y,g,z ! The 2 used in the declaration can be changed in order to create n-number of walkers, you can also limit the number of time units by altering the right-most index
 real::x
-do n=1,1000
+do n=1,1000 !You can limit the number of time units by altering the right-most index
 	position(1,n)=n
 end do
 call random_seed
 do z=2,2 !Despite the fact that this do loop is redundant with just 1 "position column", the ending value ought to be modified to the same value as the one made on line 3
 	y=1
-	do n=1,1000
+	do n=1,1000 !You can limit the number of time units by altering the right-most index
         	call random_number(x)
          	if (x .le. 1.00/3.00) then
             		position(z,n)=y+1
@@ -23,7 +23,7 @@ do z=2,2 !Despite the fact that this do loop is redundant with just 1 "position 
 	end do
 end do
 open(unit=10,file='RandomWalk.txt') !In order for the MATLAB script to work, the file name must be consistent in both .f95 and .mat files
-do g=1,1000
+do g=1,1000 !You can limit the number of time units by altering the right-most index
 	write(10,*) position(:,g)
 end do
 end program RandomWalk
