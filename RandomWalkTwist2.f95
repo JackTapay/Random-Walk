929cbc25f6a94d579c79fff5410795a66fb9e6ef
JackTapay <36042773+JackTapay@users.noreply.github.com>
2018-07-16 22:48:46 -0400

    Update RandomWalkTwist2.f95

--- RandomWalkTwist2.f95
+++ RandomWalkTwist2.f95
@@ -1,11 +1,12 @@
 SUBROUTINE Twist(n,m,y,z,position)
 	implicit none
-	INTEGER,INTENT(INOUT)::n,m,y,z,position(6,100)!The 6 used in the declaration can be changed in order to create n-number of walkers and the number of time units can be altered by changing the right-most index	if(m .gt. 6) then
-        	m=1
+	INTEGER,INTENT(INOUT)::n,m,y,z,position(6,100)!The 6 used in the declaration can be changed in order to create n-number of walkers and the number of time units can be altered by changing the right-most index	
+	if(m .gt. 6) then !The 6 within this if statement can be altered to change the maximum value
+        	m=1 !This instance of 'm' can be changed in order to alter the minimum value of the random walker
    		y=m
         	position(z,n)=m
-	else if (m .lt. 1) then
-       		m=6
+	else if (m .lt. 1) then !The value that 'm' is compared to must be the same value of 'm' in line 5
+       		m=6 !The value that 'm' is compared to must be the same value 'm' is compared to in line 4
        		y=m
    		position(z,n)=m
   	else
@@ -32,15 +33,15 @@ program RandomWalkTwist
 implicit none
 integer :: position(6,100),n,y,g,m,z,counter,e !The 6 used in the declaration can be changed in order to create n-number of walkers and the number of time units can be altered by changing the right-most index
 real::x,d
-real,dimension(100)::randomnum !The number of time units can be altered by changing the right-most index
+real,dimension(100)::randomnum !The number of time units can be altered by changing the value within the dimension
 do n=1,100 !The number of time units can be altered by changing the right-most index
 	position(1,n)=n
 end do
 call random_seed
-do z=2,6 !Despite the fact that this do loopis redundant with just 1 "position column", the ending value ought to be modified to the same value as the one made on line 3
+do z=2,6 !Despite the fact that this do loop is redundant with just 1 "position column", the ending value ought to be modified to the same value as the one made on line 3
 	y=1
 	m=0
-	do n=1,1000 !You can change the time units of the Random Walk by changing the right-most value
+	do n=1,100 !You can change the time units of the Random Walk by changing the right-most value
     	call random_number(x)
         randomnum(n)=x
        	if (x .lt. 1.00/3.00) then
@@ -58,7 +59,7 @@ end do
 do n=1,100 !The number of time units can be altered by changing the right-most index
 	call random_number(d)
 	do z=2,6 !You can change number of particles by changing the column index 
-		do e=1,6
+		do e=1,6 !The number of site can be altered by changing the right-most index
       		counter=0
       		if (position(z,n) .eq. e) then
         		counter=counter+1
